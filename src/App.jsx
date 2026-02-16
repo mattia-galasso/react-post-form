2c182acc9db90e7f01eccfb655fb9a74f0eb4a75
MattiaGalasso <galasso.mattia94@gmail.com>
2026-02-16 19:37:28 +0100

    Add axios post method and alert success request

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
+import axios from "axios";
 import { useState } from "react";
+import Alert from "./components/Alert";
 
 const initialFormData = {
   author: "",
   title: "",
   body: "",
-  publicPost: true,
+  public: false,
 };
+
+const baseURL = "https://67c5b4f3351c081993fb1ab6.mockapi.io";
 
 export default function App() {
   const [formData, setFormData] = useState(initialFormData);
+  const [statusAPI, setStatusAPI] = useState(false);
 
   const handleFormChange = (e) => {
-    console.log("Name: ", e.target.name);
-    console.log(
-      "Value: ",
-      e.target.type === "checkbox" ? e.target.checked : e.target.value,
-    );
+    const { name, value, type, checked } = e.target;
 
     setFormData({
       ...formData,
-      [e.target.name]:
-        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleFormSend = (e) => {
+    e.preventDefault();
+
+    setFormData(initialFormData);
+
+    axios.post(baseURL + "/api/posts", formData).then((response) => {
+      console.log(response);
+      setStatusAPI(true);
     });
   };
 
@@ -30,63 +41,69 @@ export default function App() {
         <div className="card my-4">
           <div className="card-header h3 text-center">Nuovo Post</div>
           <div className="card-body">
-            <form>
-              <label htmlFor="authorInput" className="col-sm-2 col-form-label">
-                Autore
-              </label>
-              <input
-                //
-                value={formData.author}
-                onChange={handleFormChange}
-                name="author"
-                //
-                type="text"
-                className="form-control"
-                id="authorInput"
-              />
-              <label htmlFor="titleInput" className="col-sm-2 col-form-label">
-                Titolo
-              </label>
-              <input
-                //
-                value={formData.title}
-                onChange={handleFormChange}
-                name="title"
-                //
-                type="text"
-                className="form-control"
-                id="titleInput"
-              />
-              <label htmlFor="bodyInput" className="col-sm-2 col-form-label">
-                Descrizione
-              </label>
-              <input
-                //
-                value={formData.body}
-                onChange={handleFormChange}
-                name="body"
-                //
-                type="text"
-                className="form-control"
-                id="bodyInput"
-              />
-              <div className="form-check my-3">
+            {statusAPI && <Alert />}
+            {statusAPI || (
+              <form onSubmit={handleFormSend}>
+                <label
+                  htmlFor="authorInput"
+                  className="col-sm-2 col-form-label"
+                >
+                  Autore
+                </label>
                 <input
                   //
-                  checked={formData.publicPost}
+                  value={formData.author}
                   onChange={handleFormChange}
-                  name="publicPost"
+                  name="author"
                   //
-                  className="form-check-input"
-                  type="checkbox"
-                  id="publicCheck"
+                  type="text"
+                  className="form-control"
+                  id="authorInput"
                 />
-                <label className="form-check-label" htmlFor="publicCheck">
-                  Post pubblico
+                <label htmlFor="titleInput" className="col-sm-2 col-form-label">
+                  Titolo
                 </label>
-              </div>
-              <button className="btn btn-primary">Aggiungi Post</button>
-            </form>
+                <input
+                  //
+                  value={formData.title}
+                  onChange={handleFormChange}
+                  name="title"
+                  //
+                  type="text"
+                  className="form-control"
+                  id="titleInput"
+                />
+                <label htmlFor="bodyInput" className="col-sm-2 col-form-label">
+                  Descrizione
+                </label>
+                <input
+                  //
+                  value={formData.body}
+                  onChange={handleFormChange}
+                  name="body"
+                  //
+                  type="text"
+                  className="form-control"
+                  id="bodyInput"
+                />
+                <div className="form-check my-3">
+                  <input
+                    //
+                    checked={formData.public}
+                    onChange={handleFormChange}
+                    name="public"
+                    //
+                    className="form-check-input"
+                    type="checkbox"
+                    id="publicCheck"
+                  />
+                  <label className="form-check-label" htmlFor="publicCheck">
+                    Post pubblico
+                  </label>
+                </div>
+                <button className="btn btn-primary">Aggiungi Post</button>
+              </form>
+            )}
           </div>
         </div>
       </div>
---
+++ src/components/Alert.jsx
@@ -0,0 +1,9 @@
+export default function Alert() {
+  return (
+    <>
+      <div className="alert alert-success" role="alert">
+        Richiesta inviata con successo!
+      </div>
+    </>
+  );
+}
