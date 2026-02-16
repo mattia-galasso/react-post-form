2491595460a0da120b663a3d7942d49c2ec43933
MattiaGalasso <galasso.mattia94@gmail.com>
2026-02-16 15:39:08 +0100

    Add Form and console input check

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# React + Vite
+# Post Form
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Nome Repo: `react-post-form`
 
-Currently, two official plugins are available:
+---
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) (or [oxc](https://oxc.rs) when used in [rolldown-vite](https://vite.dev/guide/rolldown)) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+> Oggi creeremo il nostro primo form multifield per inviare dati in POST ad un’API.
+>
+> Dovremo creare una nuova app React che contenga un form per creare un nuovo post all’interno di un blog.
+>
+> I dati che il form dovrà inviare sono i seguenti:
+>
+> - author (string) - L’autore del post
+> - title (string) - Il titolo del post
+> - body (string) - Il testo del post
+> - public (boolean) - Se il post deve essere pubblico (true) o una bozza (false)
+>
+> L’endpoint a cui effettuare la chiamata POST è il seguente: `https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts`
+>
+> Questo endpoint, in caso di successo, vi restituisce i dati inviati. Stampateli in console per verificare di essere riusciti ad inviare correttamente tutto!
 
-## React Compiler
+#### Bonus
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+> - aggiungere e gestire un alert per dare agli utenti un feedback sull’invio del form
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,94 @@
 import { useState } from "react";
 
+const initialFormData = {
+  author: "",
+  title: "",
+  body: "",
+  publicPost: true,
+};
+
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleFormChange = (e) => {
+    console.log("Name: ", e.target.name);
+    console.log(
+      "Value: ",
+      e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    );
+
+    setFormData({
+      ...formData,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    });
+  };
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="container">
+        <div className="card my-4">
+          <div className="card-header h3 text-center">Nuovo Post</div>
+          <div className="card-body">
+            <form>
+              <label htmlFor="authorInput" className="col-sm-2 col-form-label">
+                Autore
+              </label>
+              <input
+                //
+                value={formData.author}
+                onChange={handleFormChange}
+                name="author"
+                //
+                type="text"
+                className="form-control"
+                id="authorInput"
+              />
+              <label htmlFor="titleInput" className="col-sm-2 col-form-label">
+                Titolo
+              </label>
+              <input
+                //
+                value={formData.title}
+                onChange={handleFormChange}
+                name="title"
+                //
+                type="text"
+                className="form-control"
+                id="titleInput"
+              />
+              <label htmlFor="bodyInput" className="col-sm-2 col-form-label">
+                Descrizione
+              </label>
+              <input
+                //
+                value={formData.body}
+                onChange={handleFormChange}
+                name="body"
+                //
+                type="text"
+                className="form-control"
+                id="bodyInput"
+              />
+              <div className="form-check my-3">
+                <input
+                  //
+                  checked={formData.publicPost}
+                  onChange={handleFormChange}
+                  name="publicPost"
+                  //
+                  className="form-check-input"
+                  type="checkbox"
+                  id="publicCheck"
+                />
+                <label className="form-check-label" htmlFor="publicCheck">
+                  Post pubblico
+                </label>
+              </div>
+              <button className="btn btn-primary">Aggiungi Post</button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
